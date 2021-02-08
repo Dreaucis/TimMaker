fda52732c7c905106e57ac245413fd9fb1982250
Xavier Depraetere <savvesd@gmail.com>
2021-02-08 22:55:55 +0100

    Add make_tim bot command
    Added command which finds users in the voice channel and splits them up
    into teams.

---
+++ main.py
@@ -0,0 +1,73 @@
+import random
+import os
+
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='$')
+
+TEAM_NAME_ADJECTIVES = [
+    'Electrifying',
+    'Exhilarating',
+    'Delightful',
+    'Sensational',
+    'Animating',
+    'Stimulating',
+    'Vitalizing',
+    'Overjoyed',
+    'Euphoric',
+    'Jubilant',
+    'Cool',
+    'Smart'
+]
+
+TEAM_NAME_ANIMALS = [
+    'Moose (Meese? Mooses?) :thinking:',
+    'Ducks',
+    'Tigers',
+    'Elephants',
+    'Silverfishes',
+    'Cats',
+    'Penguins',
+    'Squirrels',
+    'Hedgehogs',
+    'Dogs',
+    'Pandas',
+    'Rabbits',
+    'Donkeys'
+]
+
+
+@bot.command()
+async def make_tim(ctx: commands.Context, *extra_players, number_of_teams=2, voice_channel_nr=0):
+    """ Divides the users in the channel into teams """
+    voice_channel = ctx.guild.voice_channels[voice_channel_nr]
+    users = [m.name for m in voice_channel.members] + list(extra_players)
+    users = random.sample(users, len(users))
+
+    if len(users) < 2:
+        msg = f'Tim can only see {len(users)} in the channel {voice_channel.name}. If you are more than that in ' \
+              f'the voice channel, then leaving and joining again is likely to fix it!'
+        await ctx.channel.send(msg)
+        return
+
+    teams = [[] for i in range(number_of_teams)]
+    for i, user in enumerate(users):
+        teams[i % number_of_teams].append(user)
+
+    msg = 'I, Tim, made these tims!:\n'
+    team_name_adjectives = random.sample(TEAM_NAME_ADJECTIVES, number_of_teams)
+    team_name_animals = random.sample(TEAM_NAME_ANIMALS, number_of_teams)
+    for i, team in enumerate(teams):
+        msg += f'The {team_name_adjectives[i]} {team_name_animals[i]} \n'
+        msg += f"""\t {str(team).strip("[]").replace("'", '')}\n"""
+    msg += 'GL HF!'
+
+    await ctx.channel.send(msg)
+
+
+def get_token_from_file(file: str):
+    with open(file) as f:
+        return f.read().split('=')[1]
+
+
+bot.run(os.getenv('TOKEN') or get_token_from_file('.env'))
