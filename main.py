29a16836f04669daf3c60047fa77637fc9d9139e
Xavier Depraetere <savvesd@gmail.com>
2021-02-14 17:18:53 +0100

    Add redraft button to suggest_teams
    Add emoji reaction "button" which redos the team lineups for the teams
    generated by the suggest_teams command. Move some of the logic in
    suggest_teams into helper functions for readability. Maybe a Cog should
    be made for the suggest_teams command an alike?

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 from pathlib import Path
 from functools import lru_cache
 from typing import Optional, List
+from asyncio import TimeoutError
 
 import requests
 from discord.ext import commands
@@ -39,11 +40,33 @@ def get_token_from_file(file: Path) -> str:
         return f.read().split('=')[1]
 
 
+def assign_teams(users: List[str], number_of_teams: int) -> List[List[str]]:
+    """ Randomly splits users into <number_of_teams> lists """
+    users = random.sample(users, len(users))  # Shuffle to ensure different teams
+    teams = [[] for _ in range(number_of_teams)]
+    for i, user in enumerate(users):
+        teams[i % number_of_teams].append(user)
+    return teams
+
+
+def construct_suggest_team_reply(teams: List[List[str]], adjectives: List[str], animals: List[str]) -> str:
+    """ Constructs the message for suggesting teams """
+    msg = 'I, Tim, suggest these teams!:\n'
+    for i, team in enumerate(teams):
+        msg += 'The {ADJECTIVE} {ANIMAL} \n'.format(ADJECTIVE=adjectives[i], ANIMAL=animals[i])
+        msg += '\t {TEAM_MEMBERS}\n'.format(TEAM_MEMBERS=str(team).strip("[]").replace("'", ''))
+    msg += 'GL HF!'
+    return msg
+
+
 @bot.command()
 async def suggest_teams(ctx: commands.Context, *extra_players: str):
     """
     Randomly divides users in the first voice channel into teams. Additional users can be added by typing their names
     separated by spaces.
+
+    Buttons:
+        🔄: Redraft the teams
 
     Example:
         $suggest_teams
@@ -55,32 +78,54 @@ async def suggest_teams(ctx: commands.Context, *extra_players: str):
     # Collect users in voice and args
     voice_channel = ctx.guild.voice_channels[voice_channel_nr]
     users = [m.name for m in voice_channel.members] + list(extra_players)
-    users = random.sample(users, len(users))  # Shuffle to ensure different teams
 
     # If there's are less than two users, then no teams can be constructed. Inform user and return.
     if len(users) < 2:
         msg = (
-            "I, Tim, can only see {N_USERS} in the voice channel {VOICE_CHL_NAME}. " 
+            "I, Tim, can only see {N_USERS} in the voice channel {VOICE_CHL_NAME}. "
             "If you are more than that in the voice channel, then leaving and joining again is likely to fix it!"
         ).format(N_USERS=len(users), VOICE_CHL_NAME=voice_channel.name)
         await ctx.channel.send(msg, delete_after=MSG_DELETION_TIME)
         return
 
     # Assign users to teams
-    teams = [[] for _ in range(number_of_teams)]
-    for i, user in enumerate(users):
-        teams[i % number_of_teams].append(user)
+    teams = assign_teams(users, number_of_teams)
+
+    # Created random team names. Performed here to allow for the same names after redraft
+    team_name_adjectives = random.sample(SUGGEST_TEAM_ADJECTIVES, len(teams))
+    team_name_animals = random.sample(SUGGEST_TEAM_ANIMALS, len(teams))
 
     # Construct reply
-    msg = 'I, Tim, suggest these teams!:\n'
-    team_name_adjectives = random.sample(SUGGEST_TEAM_ADJECTIVES, number_of_teams)
-    team_name_animals = random.sample(SUGGEST_TEAM_ANIMALS, number_of_teams)
-    for i, team in enumerate(teams):
-        msg += 'The {ADJECTIVE} {ANIMAL} \n'.format(ADJECTIVE=team_name_adjectives[i], ANIMAL=team_name_animals[i])
-        msg += '\t {TEAM_MEMBERS}\n'.format(TEAM_MEMBERS=str(team).strip("[]").replace("'", ''))
-    msg += 'GL HF!'
+    msg = construct_suggest_team_reply(teams, team_name_adjectives, team_name_animals)
 
-    await ctx.channel.send(msg, delete_after=MSG_DELETION_TIME)
+    # Send first team lineup
+    message = await ctx.channel.send(msg)
+
+    # Add a redraft button
+    redo_emoji = '🔄'
+    await message.add_reaction(redo_emoji)
+
+    # Wait for any redraft to be requested, if any are requested. Redraft the teams and print the new teams.
+    while True:
+        # Wait for the redraft emoji reaction. If none is given after <MSG_DELETION_TIME> sec delete message and return.
+        try:
+            reaction, user = await bot.wait_for(
+                'reaction_add',
+                timeout=MSG_DELETION_TIME,
+                check=lambda r, u:  str(r.emoji) == redo_emoji and u != message.author
+            )
+        except TimeoutError:
+            await message.delete()
+            return
+
+        await message.remove_reaction(reaction, user)  # Remove the new react to allow for multiple redrafts
+
+        # Redraft teams abd reconstruct the message
+        teams = assign_teams(users, number_of_teams)
+        msg = construct_suggest_team_reply(teams, team_name_adjectives, team_name_animals)
+
+        # Edit the message
+        await message.edit(content=msg)
 
 
 @bot.command()
